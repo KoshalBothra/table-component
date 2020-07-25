320ee87332bc592a58fa46baa3c9358d5420be75
KoshalBothra <koshalbothra@gmail.com>
2020-07-25 19:19:35 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import CustomizedTables from './table';
 function App() {
+  
+  function createData(sno, prodname, SKU, sprice, mrp,unit,type) {
+    return { sno, prodname, SKU, sprice, mrp,unit,type };
+  }
+  
+  const rows = [
+    createData(1,'Brittania White Bread','B1993932',40.00,46.00,'/Pc','Regular'),
+    createData(2,'Brittania White Bread','B1993932',40.00,46.00,'/Pc','Regular'),
+    createData(3,'Brittania White Bread','B1993932',40.00,46.00,'/Pc','Regular'),
+    createData(4,'Brittania White Bread','B1993932',40.00,46.00,'/Pc','Regular'),
+    createData(5,'Brittania White Bread','B1993932',40.00,46.00,'/Pc','Regular'),
+    createData(6,'Brittania White Bread','B1993932',40.00,46.00,'/Pc','Regular'),
+    createData(7,'Brittania White Bread','B1993932',40.00,46.00,'/Pc','Regular'),
+    createData(8,'Brittania White Bread','B1993932',40.00,46.00,'/Pc','Regular'),
+  ];
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CustomizedTables data={rows}/>
     </div>
   );
 }
---
+++ src/table.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Checkbox from "@material-ui/core/Checkbox";
+
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor:'#232C36',
+    color: '#53687C',
+    fontFamily:'Poppins',
+    fontSize: 14,
+    fontStyle:'normal',
+    alignItems:'center',
+    fontWeight:500,
+  },
+  body: {
+    fontSize: 12,
+    color: '#9FAAB6',
+    fontFamily:'Poppins',
+    fontStyle:'normal',
+    alignItems:'center',
+    fontWeight:500,
+  },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: '#232C36',
+    },
+    '&:nth-of-type(even)': {
+      backgroundColor: '#29333E',
+    }
+  },
+}))(TableRow);
+
+
+
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 700,
+  },
+});
+
+export default function CustomizedTables(props) {
+  const classes = useStyles();
+
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+          <StyledTableCell />
+          <StyledTableCell>SNo.</StyledTableCell>
+            <StyledTableCell align="left" >Product Name</StyledTableCell>
+            <StyledTableCell align="left">SKU</StyledTableCell>
+            <StyledTableCell align="left">S.Price</StyledTableCell>
+            <StyledTableCell align="left">MRP</StyledTableCell>
+            <StyledTableCell align="left" >Unit</StyledTableCell>
+            <StyledTableCell align="left" >Type</StyledTableCell>
+
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {props.data.map((row) => (
+            <StyledTableRow key={row.sno}>
+              <StyledTableCell padding="checkbox">
+                <Checkbox color='default'/>
+              </StyledTableCell>              
+                <StyledTableCell >{row.sno}</StyledTableCell>
+              <StyledTableCell align="left" >{row.prodname}</StyledTableCell>
+              <StyledTableCell align="left">{row.SKU}</StyledTableCell>
+              <StyledTableCell align="left">{row.sprice}</StyledTableCell>
+              <StyledTableCell align="left">{row.mrp}</StyledTableCell>
+              <StyledTableCell align="left">{row.unit}</StyledTableCell>
+              <StyledTableCell align="left">{row.type}</StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
